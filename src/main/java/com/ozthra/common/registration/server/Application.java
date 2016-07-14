b7e9bb0148d536e83940b835c540a48fbbbd1e70
Muruga Mani <muruga@ozthra.com>
2016-07-14 19:16:06 -0400

    Main class renamed to Application

--- src/main/java/com/ozthra/common/registration/server/Application.java
+++ src/main/java/com/ozthra/common/registration/server/Application.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistrationServer {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(RegistrationServer.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
